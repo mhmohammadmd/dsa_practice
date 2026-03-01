0dd866517df28e49379fa458aa05822f6ce56c95
Mohammad <134642626+mhmohammadmd@users.noreply.github.com>
2026-03-01 23:23:46 +0530

    Add minPartitions method to calculate deci-binary count
    Implement a solution to find the minimum number of deci-binary numbers that sum up to a given positive decimal integer.

---
+++ BinaryBitOperations/Partition-Min-nm-Decibnry-num.java
@@ -0,0 +1,42 @@
+// A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros. For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+
+// Given a string n that represents a positive decimal integer, return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+
+ 
+
+// Example 1:
+
+// Input: n = "32"
+// Output: 3
+// Explanation: 10 + 11 + 11 = 32
+// Example 2:
+
+// Input: n = "82734"
+// Output: 8
+// Example 3:
+
+// Input: n = "27346209830709182346"
+// Output: 9
+ 
+
+// Constraints:
+
+// 1 <= n.length <= 105
+
+class Solution {
+    public int minPartitions(String n) {
+        int maxDigit = 0;
+        
+        for (int i = 0; i < n.length(); i++) {
+            int digit = n.charAt(i) - '0';
+            maxDigit = Math.max(maxDigit, digit);
+            
+            // Early stopping (since max possible digit is 9)
+            if (maxDigit == 9) {
+                return 9;
+            }
+        }
+        
+        return maxDigit;
+    }
+}
