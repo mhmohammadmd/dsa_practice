791a8268e70ad904eec9cd8e09049ef37b8c248f
Mohammad <134642626+mhmohammadmd@users.noreply.github.com>
2026-03-01 23:11:18 +0530

    Add method to move zeros to the end of the array
    Implemented a method to move all zeros in the array to the end while maintaining the order of non-zero elements.

---
+++ arrays/arrays/TwoPointer/Move-all-zero-to-end.java
@@ -0,0 +1,38 @@
+// You are given an array arr[] of non-negative integers. You have to move all the zeros in the array to the right end while maintaining the relative order of the non-zero elements. The operation must be performed in place, meaning you should not use extra space for another array.
+
+// Examples:
+
+// Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+// Output: [1, 2, 4, 3, 5, 0, 0, 0]
+// Explanation: There are three 0s that are moved to the end.
+// Input: arr[] = [10, 20, 30]
+// Output: [10, 20, 30]
+// Explanation: No change in array as there are no 0s.a
+// Approach:
+//  Use two pointer
+//  first pointer is use to find the first zero
+//   after finding the first zero we store the index of that in second pointer and find the non zero element and swapped it with zero
+//   and increment the sec pointer
+  
+  class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int j=-1;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1)return ;
+        for(int i=j+1;i<n;i++){
+            if(arr[i]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+        }
+    }
+}
