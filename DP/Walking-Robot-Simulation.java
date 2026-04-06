65dc2ae898a48c5019f1af09058b3bef7c251984
Mohammad <134642626+mhmohammadmd@users.noreply.github.com>
2026-04-06 23:22:14 +0530

    Update Walking-Robot-Simulation.java

--- DP/Walking-Robot-Simulation.java
+++ DP/Walking-Robot-Simulation.java
@@ -50,3 +50,55 @@ commands[i] is either -2, -1, or an integer in the range [1, 9].
 0 <= obstacles.length <= 104
 -3 * 104 <= xi, yi <= 3 * 104
 The answer is guaranteed to be less than 231.
+
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        Set<String> set = new HashSet<>();
+        
+        // store obstacles
+        for (int[] o : obstacles) {
+            set.add(o[0] + "#" + o[1]);
+        }
+        
+        int[][] dirs = {
+            {0, 1},   // North
+            {1, 0},   // East
+            {0, -1},  // South
+            {-1, 0}   // West
+        };
+        
+        int x = 0, y = 0;
+        int d = 0; // start facing north
+        
+        int maxDist = 0;
+        
+        for (int cmd : commands) {
+            
+            if (cmd == -1) {
+                d = (d + 1) % 4; // turn right
+            } 
+            else if (cmd == -2) {
+                d = (d + 3) % 4; // turn left
+            } 
+            else {
+                // move step by step
+                for (int i = 0; i < cmd; i++) {
+                    
+                    int nx = x + dirs[d][0];
+                    int ny = y + dirs[d][1];
+                    
+                    if (set.contains(nx + "#" + ny)) {
+                        break; // obstacle found
+                    }
+                    
+                    x = nx;
+                    y = ny;
+                    
+                    maxDist = Math.max(maxDist, x*x + y*y);
+                }
+            }
+        }
+        
+        return maxDist;
+    }
+}
